c149ba3962593e96d42a5d2c41f0d29d0faf8021
Melanie Maldonado <Mal24047@byui.edu>
2026-07-22 16:08:18 -0700

    Fixed some CSS and finished Contact Us form

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const pageMenu = document.querySelector("#page-menu");
 
-pageMenu.addEventListener("change", function() {
+if (pageMenu) {
+    pageMenu.addEventListener("change", function() {
     const selectedPage = pageMenu.value;
     
     window.location.href = selectedPage;
-})
+    });
+}
+
+
+const contactForm = document.getElementById("contactForm");
+const successMessage = document.getElementById("successMessage");
+
+if(contactForm && successMessage) {
+    contactForm.addEventListener("submit", function (event){
+        event.preventDefault();
+
+        successMessage.textContent =
+        "Thank You! Your message has been recived";
+
+        contactForm.reset();
+    });
+}
